543242a27a88f9b74087ea653f30618bd266bb0b
Tchao Franki <joelxavif@gmail.com>
2023-05-23 12:00:17 +0200

    resource java

---
+++ src/main/java/be/eafcuccle/tchao/loggins/exercies/LogginsExercies.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package be.eafcuccle.tchao.loggins.exercies;
+
+/**
+ *
+ * @author joelx
+ */
+public class LogginsExercies {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Franki joel");
+    }
+}
